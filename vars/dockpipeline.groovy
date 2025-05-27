ecfbf6d3dc58786753ef945ad1d8c723e5496053
mhesham2000 <mohamedheshamde@gmail.com>
2025-05-27 18:44:59 +0300

    fileshare

--- vars/dockpipeline.groovy
+++ vars/dockpipeline.groovy
@@ -1,26 +1,35 @@
 
-def call(script){
+def call(){
 
-	script.node('agent1'){
-		script.stage('Checkout App') {
-   		   script.checkout script.scm
- 		   }
-	script.withCredentials([
-		string(credentialsId: 'dockerhub-user', variable: 'DOCKER_USER'),
-		string(credentialsId: 'dockerhub-password', variable: 'DOCKER_PASSWORD')
 
-			]){
+pipeline{
+	agent {
+		label 'agent1'
+		}
+
+	environment{
+		DOCKER_USER = credentials('dockerhub-user')
+		DOCKER_PASSWORD = credentials('dockerhub-password')
+		}
+	
+	stages{
+		stage("build Docker image"){
+			steps{
+				sh 'docker build -t mhesham2000/pythonhub:${BUILD_NUMBER} .'
+				}
+					}
+		stage("push Docker image"){
+			steps{
+				sh 'docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}'
+				sh 'docker push mhesham2000/pythonhub:${BUILD_NUMBER}'
+
+				}
+					}
+
 			
-		 script.stage('Build Docker image') {
-           		    script.sh "docker build -t mhesham2000/pythonhub:${env.BUILD_NUMBER} ."
-   					     }
+		}
+	}
 
-    	   	 script.stage('Push Docker image') {
-        		    script.sh "docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}"
-          		    script.sh "docker push mhesham2000/pythonhub:${env.BUILD_NUMBER}"
-      						  }
-		  		}
-}
 
 
 }
