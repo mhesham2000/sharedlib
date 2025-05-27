a8378a137c287d85aa7cb7219b58ecb0cc060bc2
mhesham2000 <mohamedheshamde@gmail.com>
2025-05-27 18:00:25 +0300

    fileshare

---
+++ vars/pipeline.groovy
@@ -0,0 +1,31 @@
+def call(){
+	pipeline{
+	agent {
+		label 'agent1'
+		}
+
+	environment{
+		DOCKER_USER = credentials('dockerhub-user')
+		DOCKER_PASSWORD = credentials('dockerhub-password')
+		}
+	
+	stages{
+		stage("build Docker image"){
+			steps{
+				sh 'docker build -t mhesham2000/pythonhub:${BUILD_NUMBER} .'
+				}
+					}
+		stage("push Docker image"){
+			steps{
+				sh 'docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}'
+				sh 'docker push mhesham2000/pythonhub:${BUILD_NUMBER}'
+
+				}
+					}
+
+			
+		}
+	}
+
+
+}
